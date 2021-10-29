6a3dc63e1a57d11e43bb3a315a5b4046eaa1332e
atelier des Basteks <64981563+basteks@users.noreply.github.com>
2021-10-29 22:41:55 +0200

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import json
-from geojson import FeatureCollection
-import geojsonintersect as geoJSInter
-
-with open('input.geo.json',encoding='utf-8-sig', errors='ignore') as f:
-    features = json.load(f, strict=False)["features"]
-featuresCollection = FeatureCollection(features)
-geoJSInter.add_junctions(featuresCollection)
