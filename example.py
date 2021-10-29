63236c6b9e8fa4fec85fcd7fbee310d2237a9fd4
Benjamin Hatton <atelierdesbasteks@lilo.org>
2021-10-29 23:17:08 +0200

    Reorganizing files

--- example.py
+++
@@ -1,8 +0,0 @@
-import json
-from geojson import FeatureCollection
-import geojsonintersect as geoJSInter
-
-with open('example_input.geojson',encoding='utf-8-sig', errors='ignore') as f:
-    features = json.load(f, strict=False)["features"]
-featuresCollection = FeatureCollection(features)
-geoJSInter.add_junctions(featuresCollection)
