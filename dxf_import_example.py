9f46c780af253922e34c330383c7a54d08beb6fa
Benjamin Hatton <atelierdesbasteks@lilo.org>
2021-11-10 00:15:52 +0100

    Move the plot function to geojsonintersect.py

--- dxf_import_example.py
+++ dxf_import_example.py
@@ -1,11 +1,9 @@
 import ezdxf
 from geojson import LineString, Feature, FeatureCollection
 import geojsonintersect as geoJSInter
-import shapely.geometry as shpGeom
-import geopandas as gpd
-import matplotlib.pyplot as plt
 
 ## Choose which dxf file to use
+#dxfdoc = ezdxf.readfile('examples/test.dxf')
 dxfdoc = ezdxf.readfile('examples/EN10077_H3_window_frame.dxf')
 #dxfdoc = ezdxf.readfile('examples/roof_thermal_bridge.dxf')
 
@@ -29,16 +27,4 @@ for entity in dxfdoc.entities:
 
 featuresCollection = FeatureCollection(my_features)
 junctions = geoJSInter.add_junctions(featuresCollection)
-
-shapelyFeatures = shpGeom.GeometryCollection([shpGeom.shape(feature["geometry"]) for feature in featuresCollection.features])
-gpdGeom = gpd.GeoDataFrame({'geometry':shapelyFeatures})
-
-fig = plt.figure() 
-ax = fig.gca()
-for geom in gpdGeom.geometry:
-    x, y = geom.xy
-    ax.plot(x, y, color='#999999', linewidth=1, solid_capstyle='round', zorder=1)
-for pt in junctions:
-    plt.scatter(x=pt.coords[0][0], y=pt.coords[0][1], marker='o', c='red', s=10, zorder=2)
-ax.axis('scaled')
-plt.show()
+geoJSInter.plot_geometry(featuresCollection,junctions)
